04e3a0d757c34d143cbd85235c06cc4be036de59
lineroager <linero@dtu.dk>
2021-03-22 13:43:12 +0000

    Re-introduced documentation for the random_dna function, which was lost in a previous merch.

--- R/random_dna.R
+++ R/random_dna.R
@@ -1,9 +1,14 @@
-# R/random_dna.R
+#' Create a random DNA sequence based on the wanted length
+#' 
+#' \code{random_dna} takes a number as an input argument, which is the length of the 
+#' DNA sequence, and returns a random sequence of that length.
+#' 
+#' @param l is an integer representing the length of sequence
+#' 
+#' @return returns a random DNA sequence of length \code{l}
+
 random_dna <- function(l){
   nucleotides <- sample(c("A", "T", "G", "C"), size = l, replace = TRUE)
   dna = paste0(nucleotides, collapse = "")
   return(dna)
 }
-
-
-
