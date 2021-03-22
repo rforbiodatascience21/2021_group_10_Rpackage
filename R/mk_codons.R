563c374d903a0a3e09a991c2d3aedc24344c1062
lineroager <linero@dtu.dk>
2021-03-22 09:29:09 +0000

    Added documentation to the mk_codons function. Also built an R documentation document regarding that function and added it to the man folder.

--- R/mk_codons.R
+++ R/mk_codons.R
@@ -1,3 +1,14 @@
+#' Get codons from a DNA string
+#' 
+#' \code{mk_codons} takes a DNA string and uses the \code{substring} 
+#' function to divide it into 3-letter codons. 
+#' 
+#' @param dna a string of nucleotides (only DNA, not usable on RNA), 
+#' which will be subset into codons.
+#' @param s the starting position for extracting the codons. Default is set to 1.
+#' @return returns \code{codons}, the codons extracted from the originating DNA string.
+#' 
+
 mk_codons <- function(dna, s = 1){
   l = nchar(dna)
   codons <- substring(dna,
